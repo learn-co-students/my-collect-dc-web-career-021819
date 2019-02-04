ae1707b0153403b393dcd3c255752bde0e702b29
mmorales4050 <mmorales4050@gmail.com>
2019-02-03 22:13:40 -0500

    My Collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while counter < array.length do
+    updated_item = yield(array[counter])
+    new_array << updated_item
+    counter += 1
+  end
+  new_array
+end
